867488b65a897a768727d8d4032bc729b00040ac
Mike Botsko <botsko@gmail.com>
2016-01-17 00:01:43 -0800

    Initial import

---
+++ src/main/java/com/helion3/orealerts/ChangeBlockBreakListener.java
@@ -0,0 +1,143 @@
+/**
+ * This file is part of OreAlerts, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016 Helion3 http://helion3.com/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.helion3.orealerts;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.spongepowered.api.block.BlockSnapshot;
+import org.spongepowered.api.block.BlockType;
+import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.data.Transaction;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.gamemode.GameMode;
+import org.spongepowered.api.entity.living.player.gamemode.GameModes;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.block.ChangeBlockEvent;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.channel.MessageChannel;
+import org.spongepowered.api.text.format.TextColor;
+import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+final public class ChangeBlockBreakListener {
+    private final MessageChannel channel = MessageChannel.permission("orealerts.receive");
+
+    @Listener
+    public void onChangeBlock(final ChangeBlockEvent.Break event) {
+        Optional<Player> optionalPlayer = event.getCause().first(Player.class);
+
+        // Must be player-driven
+        if (!optionalPlayer.isPresent()) {
+            return;
+        }
+
+        Player player = optionalPlayer.get();
+
+        // Skip creative players
+        Optional<GameMode> gameMode = player.getGameModeData().get(Keys.GAME_MODE);
+        if (gameMode.isPresent() && gameMode.get().equals(GameModes.CREATIVE)) {
+            return;
+        }
+
+        for (Transaction<BlockSnapshot> transaction : event.getTransactions()) {
+            if (OreAlerts.recentLocations.containsKey(transaction.getOriginal().getLocation().get())) {
+                continue;
+            }
+
+            BlockType type = transaction.getOriginal().getState().getType();
+
+            if (isWatchedOre(type)) {
+                // Count blocks in this vein
+                int matchCount = findNeighborBlocks(transaction.getOriginal()) + 1;
+
+                // Message
+                channel.send(Text.of(getColor(type), "[ore] ", player.getName(), " found " + matchCount + " ", getNiceName(type)));
+            }
+         }
+    }
+
+    private Text getNiceName(BlockType type) {
+        return Text.of(type.getId().replaceAll("minecraft:", "").replace("_ore", " "));
+    }
+
+    private TextColor getColor(BlockType type) {
+        TextColor color = TextColors.WHITE;
+
+        if (type.equals(BlockTypes.IRON_ORE)) {
+            color = TextColors.GRAY;
+        }
+        else if (type.equals(BlockTypes.LAPIS_ORE)) {
+            color = TextColors.BLUE;
+        }
+        else if (type.equals(BlockTypes.GOLD_ORE)) {
+            color = TextColors.YELLOW;
+        }
+        else if (type.equals(BlockTypes.DIAMOND_ORE)) {
+            color = TextColors.AQUA;
+        }
+
+        return color;
+    }
+
+    private boolean isWatchedOre(BlockType type) {
+        return (type.equals(BlockTypes.IRON_ORE) ||
+                type.equals(BlockTypes.GOLD_ORE) ||
+                type.equals(BlockTypes.DIAMOND_ORE) ||
+                type.equals(BlockTypes.LAPIS_ORE) ||
+                type.equals(BlockTypes.QUARTZ_ORE));
+    }
+
+    private int findNeighborBlocks(BlockSnapshot snapshot) {
+        int matchCount = 0;
+        Location<World> location = snapshot.getLocation().get();
+
+        if (OreAlerts.recentLocations.containsKey(location)) {
+            return matchCount;
+        }
+
+        OreAlerts.recentLocations.put(snapshot.getLocation().get(), new Date().getTime());
+
+        for (int x = -1; x <= 1; x++) {
+            for (int z = -1; z <= 1; z++) {
+                for (int y = -1; y <= 1; y++) {
+                    Location<World> neighborLocation = location.add(x, y, z);
+                    BlockSnapshot neighbor = neighborLocation.getBlock().snapshotFor(neighborLocation);
+
+                    if (neighbor.getState().getType().equals(snapshot.getState().getType()) && !OreAlerts.recentLocations.containsKey(neighborLocation)) {
+                        System.out.println("Vein match found " + neighborLocation.getBlockX() + " " + neighborLocation.getBlockZ() + " " + neighborLocation.getBlockY());
+                        matchCount++;
+                        if (matchCount < 30) {
+                            matchCount += findNeighborBlocks(neighbor);
+                        }
+                    }
+                }
+            }
+        }
+
+        return matchCount;
+    }
+}
---
+++ src/main/java/com/helion3/orealerts/OreAlerts.java
@@ -0,0 +1,88 @@
+/**
+ * This file is part of OreAlerts, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016 Helion3 http://helion3.com/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.helion3.orealerts;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.TimeUnit;
+
+import org.spongepowered.api.Game;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+import com.google.inject.Inject;
+
+@Plugin(id = "OreAlerts", name = "OreAlerts", version = "1.0")
+final public class OreAlerts {
+    private static Game game;
+    public static Map<Location<World>, Long> recentLocations = new HashMap<Location<World>, Long>();
+
+    /**
+     * Performs bootstrapping of Prism resources/objects.
+     *
+     * @param event Server started
+     */
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+        expireCheckedLocations();
+
+        game.getEventManager().registerListeners(this, new ChangeBlockBreakListener());
+    }
+
+    /**
+     * Injected Game instance.
+     * @param injectGame Game
+     */
+    @Inject
+    public void setGame(Game injectGame) {
+        game = injectGame;
+    }
+
+    /**
+     * Remove expired recent locations
+     */
+    public void expireCheckedLocations(){
+        game.getScheduler().createTaskBuilder()
+            .async()
+            .delay(5L, TimeUnit.MINUTES)
+            .interval(5L, TimeUnit.MINUTES)
+            .execute(new Runnable(){
+                @Override
+                public void run() {
+                    long time = new Date().getTime();
+                    for (final Entry<Location<World>, Long> entry : recentLocations.entrySet()) {
+                        final long diff = (time - entry.getValue()) / 1000;
+                        if (diff >= 300) {
+                            recentLocations.remove( entry.getKey() );
+                        }
+                    }
+                }
+            }).submit(this);
+    }
+}
