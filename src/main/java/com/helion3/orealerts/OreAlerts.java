0bee03a593519e4ee433781821e7406e9566ccf8
Mike Botsko <botsko@gmail.com>
2016-03-08 09:05:38 -0800

    Updates for SpongeAPI 4

--- src/main/java/com/helion3/orealerts/OreAlerts.java
+++ src/main/java/com/helion3/orealerts/OreAlerts.java
@@ -38,7 +38,7 @@ import org.spongepowered.api.world.World;
 
 import com.google.inject.Inject;
 
-@Plugin(id = "OreAlerts", name = "OreAlerts", version = "1.0")
+@Plugin(id = "orealerts", name = "orealerts", version = "1.0")
 final public class OreAlerts {
     private static Game game;
     public static Map<Location<World>, Long> recentLocations = new ConcurrentHashMap<Location<World>, Long>();
